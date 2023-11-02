c2109a64c9c5e5d400753853798050f1f98c8e74
Umair Ameen <umair.ameen@aspireanalytica.com>
2023-11-03 01:38:32 +0500

    registration temprary close

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,11 +64,12 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
+        return response()->json(['message' => 'Registeration is temperary close for some reason','action'=>'redirect','do'=>url('/')], 500);
+        // return User::create([
+        //     'name' => $data['name'],
+        //     'email' => $data['email'],
+        //     'password' => Hash::make($data['password']),
+        // ]);
     }
     
      /**
